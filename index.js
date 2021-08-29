c71ded975a009e4c605ba9189e5a599c9290f563
AsPulse <aspulse.x@gmail.com>
2021-08-29 16:15:56 +0000

    Add: reading target.sh

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log('Hello World!');
+const { readFileSync, existsSync } = require('fs');
+
+//--------- Console Colorlization ---------
+
+const red = (v) => `\u001b[31m${v}\u001b[0m`;
+const purple = (v) => `\u001b[35m${v}\u001b[0m`;
+
+//--------- Console Colorlization ---------
+
+const filePath = './target.sh';
+
+function main() {
+    
+    console.log(purple('[1/1] Reading target file...'));
+
+    // If "target.sh" is not provided...
+    if(!existsSync(filePath)) {
+        console.log(red('Error: The target file is not found.'))
+        process.exitCode = 1;
+        return;
+    }
+    
+    const data = readFileSync(filePath).toString();
+    console.log(data);
+
+}
+
+main();
